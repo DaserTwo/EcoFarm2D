6e091ad4cb8d6ad02e9472349813046aca68c478
DaserTwo <damiansatola@gmail.com>
2024-11-08 21:26:23 +0000

    Slot interaction

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
  * x starts from left.
  * */
 
+#define BLANC_WHITE CLITERAL(Color){ 255, 255, 255, 0 }
 #define BG_COLOR ColorFromHSV(0, 0, 0)
 
 #define LEFT_WALL 0
@@ -14,14 +15,20 @@
 #define FLOOR 300
 #define SPEED 2
 
+#define SLOT_WIDTH 200
+#define PURPLE_DURATION 300
+
 int main(){
 	InitWindow(800, 400, "EcoFarm2D");
 
 	Camera2D camera = {0};
 	camera.zoom = 1.0f;
 
-	Rectangle player = {100, FLOOR, 100, 100};
+	Rectangle player = {50, FLOOR, 100, 100};
 	int yVel = 0;
+
+	int currentSlot = -1;
+	unsigned int slotTimers[RIGHT_WALL / SLOT_WIDTH] = {0};
 
 	SetTargetFPS(60);
 	while(!WindowShouldClose()){
@@ -68,6 +75,15 @@ int main(){
 		if(camera.target.x > RIGHT_WALL - 800)
 			camera.target.x = RIGHT_WALL - 800;
 
+		currentSlot = (int)((player.x + player.width - 1) / SLOT_WIDTH);
+
+		if(currentSlot > 0 && IsKeyDown(KEY_F))
+			slotTimers[currentSlot] = PURPLE_DURATION;
+
+		for(size_t i = 0; i < RIGHT_WALL / SLOT_WIDTH; i++)
+			if(slotTimers[i])
+				slotTimers[i]--;
+
 		BeginDrawing();
 		{
 			ClearBackground(BG_COLOR);
@@ -78,7 +94,14 @@ int main(){
 			{
 				DrawLine(0, 300, 800, 300, ColorFromHSV(0, 0, 1));
 
-				for(int i = 200; i < RIGHT_WALL; i += 200){
+				for(int i = 200; i < RIGHT_WALL; i += SLOT_WIDTH){
+					int slotIndex = i / SLOT_WIDTH;
+					Color slotColor = (slotIndex == currentSlot)?GREEN:BLANK;
+					if(slotTimers[slotIndex] > 0){
+						slotColor = Fade(PURPLE, (float)slotTimers[slotIndex] / PURPLE_DURATION);
+					}
+
+					DrawRectangle(i, 0, SLOT_WIDTH, FLOOR, slotColor);
 					DrawLine(i, FLOOR, i, 0, ColorFromHSV(100, 1, 0.5f));
 				}
 
